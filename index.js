e539a9eb8873ed1fb4bee89dd9efb8f77d3eebf5
Frank-Adam <fra12023@byui.edu>
2018-01-12 23:29:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var userList = [];
+                
+function getNumbers() {
+var userInt = parseInt(prompt("Please enter any whole number"));
+                    
+    for (i = 0; i < userInt + 1; i++) {
+        userList.push(i);
+    }
+                
+    document.getElementById("userBox").innerHTML = userList.toString();
+                    
+    chngNumbers(userList);
+}
+                
+function chngNumbers(userList) {
+    alert("I'll print your list but also have a little fun with it too...")
+                    
+    for (i = 0; i < userList.length; i++) {
+        if(userList[i] % 2 == 0) {
+            userList[i] = userList[i] * i;
+        }
+    }
+    document.getElementById("userBox2").innerHTML = userList.toString();
+}
