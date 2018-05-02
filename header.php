edde3b9338e89aa20176467200a9f40a030ae979
Frank-Adam <fra12023@byui.edu>
2018-05-02 12:33:44 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
-<header class="navs">
+<?php
+echo " <header class='navs'>
 	<ul>
 		<li><a href=#>Apps</a></li>
 		<li><a href=#>Images</a></li>
 		<li><a href=#>For Fun</a></li>
 		<li><a href=#>Home</a></li>
 	</ul
-</header>
+	</header>";
+?>
